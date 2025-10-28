fe046b31251045b7afa75a39e46bccb60577da96
Yono <yonopetersen@gmail.com>
2025-10-28 10:09:32 +0100

    Initial commit

---
+++ New folder/printttt.js
@@ -0,0 +1,12 @@
+let A = 0 
+    let B = 0 
+    let C = 0
+
+    for (let A = 1; A <= 50; A++) {
+        for (let B = 1; B <= 50; B++) {
+            C = A * B
+
+        console.log(`${A} x ${B} = ${C}`);
+
+        }}
+.
